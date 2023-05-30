3248bc4bc0cdbffa88ab24c1836e091c43091edb
OBI CHIEMERIE     ("THALES") <obiwinner94@gmail.com>
2023-05-30 21:01:57 +0100

    13 fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
- * @newtree: A pointer to the root node of the tree to count the number of nodes.
+ * @newtree: pointer to root node of the tree to count the number of nodes.
  *
  * Return: If tree is NULL, the function must return 0, else return node count.
  */
